cc2956a71462cde2d7548a57c57b04f8074a03e4
Gabor Voros <gabor.voros@valtech.de>
2018-02-17 17:53:04 +0100

    adding custom footer layout to functions and widget, styling footer area

--- wp-content/themes/vantage-child/functions.php
+++ wp-content/themes/vantage-child/functions.php
@@ -6,3 +6,22 @@ function vantage_child_enqueue_parent_style() {
     wp_enqueue_style( 'vantage-parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'vantage_child_enqueue_parent_style', 8 );
+
+add_action( 'widgets_init', 'parent_override', 11 );
+function parent_override() {
+
+    unregister_sidebar('sidebar-footer');
+    /** I have looked for the ID of the sidebar by looking at
+     *  the source code in the admin.. and saw the widget's id="sidebar-4"
+     */
+
+    register_sidebar(array(
+        'name' => __( 'Footer', 'vantage' ),
+        'id' => 'sidebar-footer',
+        'description' => __( 'Fapex Cutom Footer', 'vantage' ),
+        'before_widget' => '<div class="panel-grid-cell"><div class="so-panel widget widget_circleicon-widget panel-first-child panel-last-child">',
+        'after_widget' => '</div></div>',
+        'before_title' => '<h6 class="footer-widgets-item">',
+        'after_title' => '</h6><hr>',
+    ));
+}
