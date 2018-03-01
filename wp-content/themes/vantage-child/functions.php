0a9ae6924fd94120b923ed99cd1b415de197695f
Gabor Voros <gabor.voros@valtech.de>
2018-03-01 10:41:45 +0100

    adding new tabs and refactoring the custom tab code - only 1 function takes care of the tabs

--- wp-content/themes/vantage-child/functions.php
+++ wp-content/themes/vantage-child/functions.php
@@ -16,9 +16,6 @@ function parent_override()
 {
 
     unregister_sidebar('sidebar-footer');
-    /** I have looked for the ID of the sidebar by looking at
-     *  the source code in the admin.. and saw the widget's id="sidebar-4"
-     */
 
     register_sidebar(array(
         'name' => __('Footer', 'vantage'),
@@ -31,12 +28,9 @@ function parent_override()
     ));
 }
 
-
-/* Woocommerce override*/
-
-
-
-
+/************************/
+/* Woocommerce override */
+/************************/
 
 //add contact link after short description
 add_action('woocommerce_single_product_summary', 'woocommerce_template_add_contact_link', 25);
@@ -45,15 +39,6 @@ function woocommerce_template_add_contact_link()
     echo '<a href="' . get_permalink(4) . '">Contact</a>';
 }
 
-/*add_filter('woocommerce_product_tabs', 'woo_rename_tabs', 98);
-function woo_rename_tabs($tabs)
-{
-
-    $tabs['additional_information']['title'] = __('Specification');    // Rename the additional information tab
-
-    return $tabs;
-
-}*/
 
 // Remove the additional information tab
 add_filter('woocommerce_product_tabs', 'woo_remove_product_tabs', 98);
@@ -65,54 +50,83 @@ function woo_remove_product_tabs($tabs)
 
 }
 
-//remove h2 heading from tabs
-//add_filter('woocommerce_product_description_heading', '__return_null');
-
-//add new tabs
-add_filter('woocommerce_product_tabs', 'woo_new_product_tab');
-function woo_new_product_tab($tabs)
+//add custom tabs
+add_filter('woocommerce_product_tabs', 'fapex_new_product_tabs');
+function fapex_new_product_tabs($tabs)
 {
     global $product;
+    $post_id = get_the_ID();
 
     if (!empty($product->get_attributes()['specification'])) {
         $tabs['specification_tab'] = array(
             'title' => __('Specification', 'woocommerce'),
             'priority' => 50,
-            'callback' => 'woo_specification_tab_content'
+            'callback' => 'fapex_specification_tab_content'
         );
     }
 
-    $post_id = get_the_ID();
     $uses = get_post_meta($post_id, 'wpcf-uses', true);
     if (!empty($uses)) {
         $tabs['uses_tab'] = array(
             'title' => __('Uses', 'woocommerce'),
             'priority' => 51,
-            'callback' => 'woo_uses_tab_content'
+            'callback' => 'fapex_custom_tab_content',
+            'callback_parameters' => array('wpcf-uses', $post_id)
         );
     }
+
+    $instructions = get_post_meta($post_id, 'wpcf-instructions', true);
+    if (!empty($instructions)) {
+        $tabs['instructions_tab'] = array(
+            'title' => __('Instructions', 'woocommerce'),
+            'priority' => 52,
+            'callback' => 'fapex_custom_tab_content',
+            'callback_parameters' => array('wpcf-instructions', $post_id)
+        );
+    }
+
+    $faqs = get_post_meta($post_id, 'wpcf-faqs', true);
+    if (!empty($faqs)) {
+        $tabs['faqs_tab'] = array(
+            'title' => __('FAQs', 'woocommerce'),
+            'priority' => 53,
+            'callback' => 'fapex_custom_tab_content',
+            'callback_parameters' => array('wpcf-faqs', $post_id)
+        );
+    }
+
+    $productRange = get_post_meta($post_id, 'wpcf-product-range', true);
+    if (!empty($productRange)) {
+        $tabs['productRange_tab'] = array(
+            'title' => __('Product Range', 'woocommerce'),
+            'priority' => 54,
+            'callback' => 'fapex_custom_tab_content',
+            'callback_parameters' => array('wpcf-product-range', $post_id)
+        );
+    }
+
     return $tabs;
 }
 
-function woo_specification_tab_content(){
+function fapex_specification_tab_content(){
     global $product;
-    do_action('woocommerce_product_specification', $product);
+    do_action('fapex_product_specification', $product);
 }
 
-add_action('woocommerce_product_specification', 'wc_display_specification', 10);
-function wc_display_specification($product){
+add_action('fapex_product_specification', 'fapex_display_specification', 10);
+function fapex_display_specification($product){
     set_query_var('product', $product);
     set_query_var('attribute', $product->get_attributes()['specification']);
     get_template_part('templates/single-product/product-attributes');
 }
 
-function woo_uses_tab_content(){
-    global $product;
-    do_action('woocommerce_product_uses', $product);
+function fapex_custom_tab_content($tab_name, $tab){
+    $metaParam = $tab['callback_parameters'][0];
+    $post_id = $tab['callback_parameters'][1];
+    do_action('fapex_product_customtabs', $metaParam, $post_id);
 }
 
-add_action('woocommerce_product_uses', 'wc_display_uses', 11);
-function wc_display_uses($product){
-    $post_id = get_the_ID();
-    echo get_post_meta($post_id, 'wpcf-uses', true);
+add_action('fapex_product_customtabs', 'fapex_display_customtabs', 11, 2);
+function fapex_display_customtabs($metaParam, $post_id){
+    echo get_post_meta($post_id, $metaParam, true);
 }
