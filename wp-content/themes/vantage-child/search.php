fc1e52d80fda36efd81db1b849ef7fdfeca19978
Gabor Voros <gabor.voros@valtech.de>
2018-03-13 16:01:17 +0100

    adding custom search and no-result templates

--- wp-content/themes/vantage-child/search.php
+++ wp-content/themes/vantage-child/search.php
@@ -10,59 +10,62 @@
 get_header(); ?>
 
 <section id="primary" class="content-area">
-	<div id="content" class="site-content" role="main">
+	<div id="content" class="site-content search-results" role="main">
+		<div class="search-results__page">
 
-	<?php if ( have_posts() ) : ?>
 
-		<header class="page-header">
-			<?php if ( siteorigin_page_setting( 'page_title' ) ) : ?>
-				<h1 id="page-title"><?php printf( __( 'Search Results for: %s', 'vantage' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			<?php endif; ?>
-		</header><!-- .page-header -->
+          <?php if (have_posts()) : ?>
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
+				 <h1><?php echo __('On Pages', 'vantage'); ?></h1>
+				 <header class="page-header">
+                 <?php if (siteorigin_page_setting('page_title')) : ?>
+						  <h2 id="page-title"><?php printf(__('Search Results on Pages for: %s', 'vantage'), '<span>' . get_search_query() . '</span>'); ?></h2>
+                 <?php endif; ?>
+				 </header><!-- .page-header -->
 
-			<?php get_template_part( 'content' ); ?>
+              <?php /* Start the Loop */ ?>
+              <?php while (have_posts()) : the_post(); ?>
 
-		<?php endwhile; ?>
+                  <?php get_template_part('content'); ?>
 
-		<?php vantage_content_nav( 'nav-below' ); ?>
+              <?php endwhile; ?>
 
-	<?php else : ?>
+              <?php vantage_content_nav('nav-below'); ?>
 
-		<?php get_template_part( 'no-results', 'search' ); ?>
+          <?php else : ?>
 
-	<?php endif; ?>
-		 <?php
-       wp_reset_postdata();
-		 ?>
-		 <?php
-		 $args = fapex_search_args(get_search_query());
-      /* $args = array(
-           'post_type'     => 'product',
-           'meta_key'      => 'wpcf-instructions',
-           'meta_value'        => 'Instructions',
-           'meta_compare'  => 'LIKE' );*/
+              <?php get_template_part('templates/search/no-results', 'search'); ?>
 
-       // The Query
-       $the_query = new WP_Query( $args );
+          <?php endif; ?>
 
-       // The Loop
-       if ( $the_query->have_posts() ) {
+		</div>
 
-           while ( $the_query->have_posts() ) : $the_query->the_post();
-           echo 'hejj';
-               get_template_part( 'content' );
-           endwhile;
+		<div class="search-results__product">
+          <?php wp_reset_postdata(); ?>
+          <?php $args = fapex_search_args(get_search_query()); ?>
+          <?php $the_query = new WP_Query($args); ?>
 
-       } else {
-           echo 'hoo';
-       }
-       /* Restore original Post Data */
-       wp_reset_postdata();
+		</div>
+       <?php if ($the_query->have_posts()) : ?>
 
-		 ?>
+			 <h1><?php echo __('In Products', 'vantage'); ?></h1>
+			 <header class="page-header">
+              <?php if (siteorigin_page_setting('page_title')) : ?>
+					  <h2 id="page-title"><?php printf(__('Search Results in Products for: %s', 'vantage'), '<span>' . get_search_query() . '</span> '); ?></h2>
+              <?php endif; ?>
+			 </header><!-- .page-header -->
+
+           <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+               <?php get_template_part('templates/search/search_content_product'); ?>
+
+           <?php endwhile; ?>
+
+       <?php else : ?>
+			 hoo
+       <?php endif; ?>
+
+       <?php wp_reset_postdata(); ?>
 
 	</div><!-- #content .site-content -->
 </section><!-- #primary .content-area -->
